9702cafc71719ea3df9ffb041592332d4ae53f95
lapthuan <lapthuan0805@gmail.com>
2024-06-29 19:56:42 +0700

    a

--- src/components/submit/submitDevice.js
+++ src/components/submit/submitDevice.js
@@ -1,14 +1,14 @@
-import { Button, Popconfirm, Space, message } from 'antd';
-import { useEffect, useState } from 'react';
+import { Button, Input, Popconfirm, Space, message } from 'antd';
+import { useEffect, useRef, useState } from 'react';
 import useAsync from '../../hook/useAsync';
 import { useForm } from 'antd/es/form/Form';
-import { QuestionCircleOutlined } from '@ant-design/icons';
+import { QuestionCircleOutlined, SearchOutlined } from '@ant-design/icons';
 import ServiceIp from '../../service/ServiceIp';
 import ServiceVlanIMS from '../../service/ServiceVlanIMS';
 import ServiceVlanMyTV from '../../service/ServiceVlanMyTV';
 import ServiceVlanNet from '../../service/ServiceVlanNet';
 import ServiceDevice from '../../service/ServiceDevice';
-
+import Highlighter from 'react-highlight-words';
 const SubmitDevice = () => {
     const [dataTable, setData] = useState([])
     const [editTab, setEditTab] = useState(false)
@@ -20,8 +20,112 @@ const SubmitDevice = () => {
     const { data: dataVlanIMS, loading: loadingVlanIMS } = useAsync(() => ServiceVlanIMS.getAllVlanIMS())
     const { data: dataVlanMyTV, loading: loadingVlanMyTV } = useAsync(() => ServiceVlanMyTV.getAllVlanMyTV())
     const { data: dataVlanNet, loading: loadingVlanNet } = useAsync(() => ServiceVlanNet.getAllVlanNet())
-
-
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+        confirm();
+        setSearchText(selectedKeys[0]);
+        setSearchedColumn(dataIndex);
+    };
+    const handleReset = (clearFilters) => {
+        clearFilters();
+        setSearchText('');
+    };
+    const getColumnSearchProps = (dataIndex) => ({
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+            <div
+                style={{
+                    padding: 8,
+                }}
+                onKeyDown={(e) => e.stopPropagation()}
+            >
+                <Input
+                    ref={searchInput}
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{
+                        marginBottom: 8,
+                        display: 'block',
+                    }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        icon={<SearchOutlined />}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Search
+                    </Button>
+                    <Button
+                        onClick={() => clearFilters && handleReset(clearFilters)}
+                        size="small"
+                        style={{
+                            width: 90,
+                        }}
+                    >
+                        Reset
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            confirm({
+                                closeDropdown: false,
+                            });
+                            setSearchText(selectedKeys[0]);
+                            setSearchedColumn(dataIndex);
+                        }}
+                    >
+                        Filter
+                    </Button>
+                    <Button
+                        type="link"
+                        size="small"
+                        onClick={() => {
+                            close();
+                        }}
+                    >
+                        close
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered) => (
+            <SearchOutlined
+                style={{
+                    color: filtered ? '#1677ff' : undefined,
+                }}
+            />
+        ),
+        onFilter: (value, record) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        onFilterDropdownOpenChange: (visible) => {
+            if (visible) {
+                setTimeout(() => searchInput.current?.select(), 100);
+            }
+        },
+        render: (text) =>
+            searchedColumn === dataIndex ? (
+                <Highlighter
+                    highlightStyle={{
+                        backgroundColor: '#ffc069',
+                        padding: 0,
+                    }}
+                    searchWords={[searchText]}
+                    autoEscape
+                    textToHighlight={text ? text.toString() : ''}
+                />
+            ) : (
+                text
+            ),
+    });
     useEffect(() => {
         const datatab = data.map((item, i) => {
             return {
@@ -165,30 +269,57 @@ const SubmitDevice = () => {
         {
             title: 'Loại thiết bị',
             dataIndex: 'loaithietbi',
-            sorter: (a, b) => a.name.localeCompare(b.name),
-            sortDirections: ['descend', 'ascend'],
+            key: 'loaithietbi',
+            filters: [
+                {
+                    text: 'GPON HW',
+                    value: 'GPON HW',
+                },
+                {
+                    text: 'GPON ZTE',
+                    value: 'GPON ZTE',
+                },
+                {
+                    text: 'GPON MINI ZTE',
+                    value: 'GPON MINI ZTE',
+                },
+                {
+                    text: 'GPON ALU',
+                    value: 'GPON ALU',
+                },
+            ],
+        
+            onFilter: (value, record) => record.loaithietbi.includes(value),
         },
         {
             title: 'Tên thiết bị',
             dataIndex: 'tenthietbi',
-            sorter: (a, b) => a.name.localeCompare(b.name),
-            sortDirections: ['descend', 'ascend'],
+            key: 'tenthietbi',
+            ...getColumnSearchProps('tenthietbi'),
         },
         {
             title: 'Ip',
             dataIndex: 'ipaddress',
+            key: 'ipaddress',
+            ...getColumnSearchProps('ipaddress'),
         },
         {
             title: 'Vlan IMS',
             dataIndex: 'vlanims',
+            key: 'vlanims',
+            ...getColumnSearchProps('vlanims'),
         },
         {
             title: 'Vlan MyTV',
             dataIndex: 'vlanmytv',
+            key: 'vlanmytv',
+            ...getColumnSearchProps('vlanmytv'),
         },
         {
             title: 'Vlan Net',
             dataIndex: 'vlannet',
+            key: 'vlannet',
+            ...getColumnSearchProps('vlannet'),
         },
         {
             title: "Công cụ",
