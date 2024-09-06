9be706358836b33f9519177b49c358c19956b7a4
QuocDat1219 <lenguyenquocdat1219@gmail.com>
2024-09-06 20:13:01 +0700

    fix: edit login err

--- src/pages/Sigin.js
+++ src/pages/Sigin.js
@@ -32,7 +32,6 @@ const SignIn = () => {
       const res = await ServiceUser.userLogin(data);
       if (res.token.access_token) {
         const token = res.token.access_token;
-        console.log(token);
         await cookies.set("token", token);
         history.push("/"); // Chuyển hướng sau khi đăng nhập thành công
         message.success("Đăng nhập thành công");
